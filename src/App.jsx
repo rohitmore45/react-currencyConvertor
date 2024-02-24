0dc6af46da54255018ff100d047346437ec0d70c
Rohit More <rohitbmore2002@gmail.com>
2024-02-24 12:27:13 +0530

    issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
                 label="From"
                 amount={amount}
                 currencyOptions={options}
-                onCurrencyChange={(currency) => setAmount(amount)}
+                onCurrencyChange={(currency) => setFrom(currency)}
                 selectCurrency={from}
                 onAmountChange={(amount) => setAmount(amount)}
               />
@@ -61,7 +61,7 @@ function App() {
                 amount={convertedAmount}
                 currencyOptions={options}
                 onCurrencyChange={(currency) => setTo(currency)}
-                selectCurrency={from}
+                selectCurrency={to}
                 amountDisable
               />
             </div>
